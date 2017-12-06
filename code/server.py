de465428480444922d6ca8769bbff6d7ff28d914
Carlos Treminio <ctreminiom079@gmail.com>
2017-12-06 00:49:37 -0600

    add new endpoint that create a new game sesion with the access token.

--- code/server.py
+++ code/server.py
@@ -4,16 +4,13 @@
 from flask import Flask
 from flask_restful import Api
 
-from app.controllers.game import Create, Life
-from app.controllers.movement import Movement
+
 from app.controllers.auth import Register, Login
+from app.controllers.game import Create, Join, Report
 
-
-from app.controllers.player import Prueba
 
 from db import init_db
 from flask_cors import CORS
-
 
 app = Flask(__name__)
 
@@ -24,19 +21,19 @@ api = Api(app)
 init_db()
 
 
-def addPrefix(route):
-    return '/api/v1.1.0/{}'.format(route)
-
-api.add_resource(Register, '/register')
-api.add_resource(Login, '/login')
-
-api.add_resource(Create, addPrefix('game/init/'))
-api.add_resource(Life, addPrefix('game/put/life/'))
 
 
-api.add_resource(Prueba, '/prueba')
+def add_prefix(route):
+    return '/api/v1.2.0/{}'.format(route)
 
-api.add_resource(Movement, addPrefix('game/request/movement/'))
+api.add_resource(Register, add_prefix('auth/register')) #/api/v1.2.0/auth/register
+api.add_resource(Login, add_prefix('auth/login'))       #/api/v1.2.0/auth/login
+
+api.add_resource(Create, add_prefix('game/sesion/create'))  #/api/v1.2.0/game/sesion/create
+api.add_resource(Join, add_prefix('game/sesion/join'))      #/api/v1.2.0/game/sesion/join
+
+api.add_resource(Report, add_prefix('game/reports/get/game')) #/api/v1.2.0/game/reports/get/game
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', debug=True)
